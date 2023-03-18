15607a1e67e484d74142575e4896351352bf54c0
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-03-18 13:08:28 -0700

    added consts for each employee's prompts and menu prompt

--- index.js
+++ index.js
@@ -8,51 +8,86 @@ function writeToFile(fileName, data) {
     );
 }
 
-function init() {
-    inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'managerName',
-        message: "Enter the name of the Team Manager",
-      },
-      {
-        type: 'input',
-        name: 'managerId',
-        message: "Enter the employeeID of the Team Manager",
-      },
-      {
-        type: 'input',
-        name: 'managerEmail',
-        message: "Enter the email address of the Team Manager",
-      },
-      {
-        type: 'input',
-        name: 'officeNumber',
-        message: "Enter the office number",
-      },
-      {
-        type: 'list',
-        message: "Menu",
-        name: 'optionsMenu',
-        choices: ['Add Engineer or Intern', 'Finish building team'],
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: "What is your GitHub username?",
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: "What is your email address?",
-      },
-    ])
-    .then((data) => {
-      // call to function that takes data and formats it for the markdown file named 'README'
-      writeToFile('README.md', data);
-    });
+const employeePrompt = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Enter the name of the employee",
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "Enter the employeeID of the employee",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Enter the email address of the employee",
   }
+]
+
+const managerPrompt = [
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "Enter the manager's office number",
+  }
+]
+
+const engineerPrompt = [
+  {
+    type: 'input',
+    name: 'github',
+    message: "Enter the engineer's GitHub username"
+  }
+]
+
+const internPrompt = [
+  {
+    type: "input",
+    name: "school",
+    message: "Enter the intern's school"
+  }
+]
+
+const menuPrompt = [
+  {
+    type: 'list',
+    message: "Menu",
+    name: 'menu',
+    choices: ['Add an Engineer', 'Add an Intern', 'Finish building team']
+  }
+]
+
+// function init() {
+//   inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'managerName',
+//       message: "Enter the name of the Team Manager",
+//     },
+//     {
+//       type: 'input',
+//       name: 'managerId',
+//       message: "Enter the employeeID of the Team Manager",
+//     },
+//     {
+//       type: 'input',
+//       name: 'managerEmail',
+//       message: "Enter the email address of the Team Manager",
+//     },
+//     {
+//       type: 'input',
+//       name: 'officeNumber',
+//       message: "Enter the office number",
+//     },
+//   ])
+//     .then((data) => {
+//       // call to function that takes data and formats it for the markdown file named 'README'
+//       writeToFile('README.md', data);
+//     });
+//   }
   
-  // Function call to initialize app
-  init();
+//   // Function call to initialize app
+//   init();
