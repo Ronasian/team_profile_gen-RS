08995f57c2be6352e2282252d21ea6238d077420
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-03-24 21:42:15 -0700

    added test cases for employee contructor with jest

---
+++ tests/Engineer.test.js
@@ -0,0 +1,42 @@
+const Engineer = require("../lib/engineer");
+
+describe("Engineer", () => {
+  // TODO: Add a comment describing the purpose of the following test suite
+  describe("Initialization", () => {
+    it("should create an object with a name, id, and email if provided valid arguments", () => {
+      const employee = new Employee('Robert', 3, 'robert@gmail.com');
+
+      // TODO: Add a comment describing the purpose of the following statements
+      expect(employee.name).toEqual("Robert");
+      expect(employee.id).toEqual(3);
+      expect(employee.email).toEqual("robert@gmail.com");
+    });
+
+    it("should throw an error if id is not a number", () => {
+      const cb = () => new Employee("Robert", 'two', 'robert@gmail.com');
+
+      // TODO: Add a comment describing the purpose of the following declaration
+      const err = new Error("Expected parameter 'id' to be a non-negative number");
+
+      // TODO: Add a comment describing the purpose of the following statement
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if email input is not a string", () => {
+        const cb = () => new Employee("Robert", 3, 6);
+  
+        // TODO: Add a comment describing the purpose of the following declaration
+        const err = new Error("Expected parameter 'email' to be a non empty string");
+  
+        // TODO: Add a comment describing the purpose of the following statement
+        expect(cb).toThrowError(err);
+      });
+
+    it("should throw an error if 'id' is less than 0", () => {
+      const cb = () => new Employee("Sarah", -1, "robert@gmail.com");
+      const err = new Error("Expected parameter 'id' to be a non-negative number");
+
+      expect(cb).toThrowError(err);
+    });
+  });
+});
